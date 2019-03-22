830cc389329e4eed8cceb0e8b56abd15093b9867
tionote <48472698+tionote@users.noreply.github.com>
2019-03-22 20:14:16 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+
+    $kon = mysqli_connect("localhost", "id4103787_oktioryan_0140",'billabong45',"id4103787_raport_6771");
+    if (mysqli_connect_errno()) {
+        echo "Koneksi database gagal : " .mysqli_connect_error();
+    }
+?>
